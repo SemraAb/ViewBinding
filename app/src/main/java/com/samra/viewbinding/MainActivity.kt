c349639d8c565f0be3d0df4150f0d6a6dda14394
Samra Abdurahmanli <samra.abdurahmanli.a@asoiu.edu.az>
2023-08-02 16:21:38 +0400

    first commit

---
+++ app/src/main/java/com/samra/viewbinding/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.samra.viewbinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.samra.viewbinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
